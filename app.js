9da18dcfddc0961b85795409cb6f41efe8ae201d
Toon Ketels <toon@puresign.be>
2012-07-28 12:47:24 +0200

    Added necesary modules.

--- app.js
+++ app.js
@@ -2,36 +2,28 @@
 /**
  * Module dependencies.
  */
+ var application_root = __dirname,
+    express = require("express")
+    , path = require("path")
+    , mongoose = require("mongoose");
 
-var express = require('express')
-  , routes = require('./routes')
+var app = express.createServer();
 
-var app = module.exports = express.createServer();
+// Database
+mongoose.connect('mongodb://localhost/store');
 
-// Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'c');
+// Config
+app.configure(function () {
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.cookieParser());
-  app.use(express.session({ secret: 'your secret here' }));
   app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+  app.use(express.static(path.join(application_root, "public")));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+app.get('/api', function (req, res) {
+  res.send('Ecomm API is running');
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
-
-// Routes
-
-app.get('/', routes.index);
-
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+// Launch server
+app.listen(3000); 
