6e34b38ba63638fcdd9de0c76f05c6610acc4a27
Toon Ketels <toon@puresign.be>
2012-07-28 17:39:42 +0200

    Added ProductLstView.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -18,5 +18,25 @@ Store.App = Backbone.Router.extend({
   	  Store.products.deferred.done(function(){
   	  	  productsList.render();
   	  });
-  }
+    }
+});
+
+
+Store.ProductListView = Backbone.View.extend({
+    tagName: 'ul'
+  , className: 'products'
+  , render: function() {
+  		for (var i = 0; i < this.collection.length; i++) {
+			this.renderItem(this.collection.models[i]);  			
+  		}
+  		$(this.container).find(this.className).remove();
+  		this.$el.appendTo(this.options.container);
+  		return this;
+    }
+  , renderItem: function( model ) {
+  		var item = new Store.ProductListItemView({
+  			"model": model
+  		});
+  		item.render().$el.appendTo(this.$el);
+    }
 });
