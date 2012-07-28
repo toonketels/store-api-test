5196243087f4a0ff2587c477a96a1987cb99ddbb
Toon Ketels <toon@puresign.be>
2012-07-28 17:24:13 +0200

    Created routes for backbone app.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,22 @@
-(function( $ ) {
+// Global object to capture our code so
+// we don't pollute global scope.
+Store = window.Store || {};
 
-
-
-})( jQuery );
+Store.App = Backbone.Router.extend({
+	// Define our routes...
+    routes: {
+    	'/': 'listProducts'
+      , 'list': 'listProducts'
+    }
+    // Routes callback functions...
+  , listProducts: function() {
+  	  var productsList = new Store.ProductListView({
+  	  	  'container': $.('#container')
+  	  	, 'collection': Store.products
+  	  });
+  	  // Render will be called when data is fetched
+  	  Store.products.deferred.done(function(){
+  	  	  productsList.render();
+  	  });
+  }
+});
