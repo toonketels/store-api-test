457f220d081a3904157a5446d6d9363f8f144478
Toon Ketels <toon@puresign.be>
2012-07-28 13:22:19 +0200

    Added some paths.

--- app.js
+++ app.js
@@ -4,8 +4,8 @@
  */
  var application_root = __dirname,
     express = require("express")
-    , path = require("path")
-    , mongoose = require("mongoose");
+  , path = require("path")
+  , mongoose = require("mongoose");
 
 var app = express.createServer();
 
@@ -21,8 +21,50 @@ app.configure(function () {
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
+var Schema = mongoose.Schema;
+
+var Product = new Schema({
+    title: { type: String, required: true }
+  , description: { type: String, required: true }
+  , style: { type: String, unique: true }
+  , modified: { type: Date, default: Date.now }
+});
+
+var ProductModel = mongoose.model('Product', Product);
+
+// View all products
+app.get('/api/products', function(req, res) {
+  return ProductModel.find(function(err, products) {
+    if(!err) {
+      return res.send(products);
+    } else {
+      console.log(err);
+    }
+  });
+});
+
 app.get('/api', function (req, res) {
-  res.send('Ecomm API is running');
+  res.send('Store API is running');
+});
+
+// Create a product
+app.post('/api/products', function(req, res) {
+  var product;
+  console.log('POST: ');
+  console.log(req.body);
+  product = new ProductModel({
+      title: req.body.title
+    , description: req.body.description
+    , style: req.body.style
+  });
+  product.save(function(err) {
+    if (!err) {
+      return console.log('Created!');
+    } else {
+      return console.log(err);
+    }
+  });
+  return res.send(product);
 });
 
 // Launch server
