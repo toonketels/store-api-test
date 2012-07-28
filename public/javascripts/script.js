87aeedb9938ea97b8bf8d6383439a646df070f7d
Toon Ketels <toon@puresign.be>
2012-07-28 15:07:01 +0200

    Added jquery plugin functions for each of the uri actions.

---
+++ public/javascripts/script.js
@@ -0,0 +1,136 @@
+(function( $ ) {
+
+  /**
+   * Helper function to post products.
+   * Only for during development...
+   */
+  $.fn.postProducts = function(product) {
+
+    // Provide default product...
+    if (!product) {
+      var product = {
+          "title": "Superdupe T-shirt"
+        ,  "description": "All about the details. Of course it's black."
+        ,  "images": [
+          {
+              kind: 'detail'
+            , url: 'images/animated-glowing-wifi-signal-strength-detecting-t-shirt.jpg'
+          }
+          , {
+          	kind: 'detail'
+            , url: 'images/t_shirt_t_post_abonnement.jpg'
+          }
+        ]
+        ,  "style": "7576534D2"
+      };
+    }
+    
+    // Perform post request...
+    $.post("/api/products",
+       product
+     , function (data, textStatus, jqXHR) {
+      console.log("Post resposne:"); 
+      console.dir(data); 
+      console.log(textStatus); 
+      console.dir(jqXHR);
+    });
+
+  };
+
+
+  /**
+   * Helper function to update existing product,
+   * written (as a bad) jquery plugin.
+   */
+  $.fn.putProductsWithId = function( id, product ) {
+
+    if ( !id ) {
+      var id = '5013dd3627d31e1308000003';
+    }
+
+    if (!product ) {
+      var product = {
+          "title": "My Awesome T-shirt in Black"
+        , "description": "All about the details. Of course it's black, and long sleeve"
+        ,  "images": [
+        {
+            kind: 'detail'
+          , url: 'images/animated-glowing-wifi-signal-strength-detecting-t-shirt.jpg'
+        }
+        , {
+        	kind: 'detail'
+          , url: 'images/t_shirt_t_post_abonnement.jpg'
+        }
+      ]
+        , "style": "75764"
+      };
+    }
+
+    $.ajax({
+        url: "/api/products/" + id
+      , type: "PUT"
+      , data: product
+      , success: function (data, textStatus, jqXHR) {
+        console.log("Post resposne:");
+        console.dir(data);
+        console.log(textStatus);
+        console.dir(jqXHR);
+      }
+    }); 
+  };
+
+
+  /**
+   * Get all products.
+   */
+  $.fn.getProducts = function() {
+
+  	$.get("/api/products/", function (data, textStatus, jqXHR) {
+      console.log("Get responses:");
+      console.dir(data);
+      console.log(textStatus);
+      console.dir(jqXHR);
+    }); 
+  };
+
+  /**
+   * Get a specific product.
+   */
+  $.fn.getProductsWithId = function( id ) {
+
+    if ( !id ) {
+      var id = '5013dd3627d31e1308000003';
+    }
+
+    $.get("/api/products/" + id, function(data, textStatus, jqXHR) {  
+      console.log("Get resposne:");
+      console.dir(data);
+      console.log(textStatus);
+      console.dir(jqXHR);
+    });
+  };
+
+
+  /**
+   * Delete a given product.
+   */
+  $.fn.deleteProductsWithId = function( id ) {
+
+    if ( !id ) {
+      var id = '5013dd3627d31e1308000003';
+    }
+
+    jQuery.ajax({
+        url: "/api/products/" + id
+      , type: "DELETE"
+      , success: function (data, textStatus, jqXHR) { 
+          console.log("Post resposne:"); 
+          console.dir(data); 
+          console.log(textStatus); 
+          console.dir(jqXHR); 
+      }
+    });
+  }
+
+
+})( jQuery );
